6cfe84ee9e49c7de33dbc81d011a153f8ff4711b
archibate <1931127624@qq.com>
2025-05-17 18:39:35 +0800

    arduino car add

--- ArduinoCar/ArduinoCar.ino
+++ ArduinoCar/ArduinoCar.ino
@@ -1,23 +1,31 @@
 #include <Servo.h>
 
+const int IRMETER = 12;  // 红外线传感器连到的引脚
+const int SERVOL = 11;  // 左舵机连到的引脚
+const int SERVOR = 10;  // 右舵机连到的引脚
+const int LRBALANCE = 94; // 需要根据左右舵机实际速度情况来调整，调到两边速度一致即可
+const int SPEED = 8;  // 舵机运动速度，0到90
+
+
 Servo servoL;
 Servo servoR;
 
 void setup() {
-  servoL.attach(10);
-  servoR.attach(11);
-  pinMode(4, INPUT);
+  servoL.attach(SERVOL);
+  servoR.attach(SERVOR);
+  pinMode(IRMETER, INPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  int speedL = 5;
-  int speedR = 5;
-  bool obstacled = digitalRead(4) == 0;
+  int speedL = SPEED;
+  int speedR = SPEED;
+  bool obstacled = digitalRead(IRMETER) == 0;
   if (obstacled) {
-    speedR = -5;
+    speedR = -SPEED;
+    speedL = 0;
   }
-  servoL.write(90 + speedL);
-  servoR.write(90 - speedR);
+  servoL.write(LRBALANCE + speedL);
+  servoR.write(LRBALANCE - speedR);
   delay(20);
 }
