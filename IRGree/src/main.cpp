0ac7e6d29e7a0eeea4840b49bff410b4c980f401
archibate <1931127624@qq.com>
2025-05-31 17:25:22 +0800

    gree test ok

--- IRGree/src/main.cpp
+++ IRGree/src/main.cpp
@@ -1,79 +1,65 @@
-/* Copyright 2016, 2018 David Conran
-*  Copyright 2020 Sadid Rafsun Tulon
-*
-* An IR LED circuit *MUST* be connected to the ESP8266 on a pin
-* as specified by kIrLed below.
-*
-* TL;DR: The IR LED needs to be driven by a transistor for a good result.
-*
-* Suggested circuit:
-*     https://github.com/crankyoldgit/IRremoteESP8266/wiki#ir-sending
-*
-* Common mistakes & tips:
-*   * Don't just connect the IR LED directly to the pin, it won't
-*     have enough current to drive the IR LED effectively.
-*   * Make sure you have the IR LED polarity correct.
-*     See: https://learn.sparkfun.com/tutorials/polarity/diode-and-led-polarity
-*   * Typical digital camera/phones can be used to see if the IR LED is flashed.
-*     Replace the IR LED with a normal LED if you don't have a digital camera
-*     when debugging.
-*   * Avoid using the following pins unless you really know what you are doing:
-*     * Pin 0/D3: Can interfere with the boot/program mode & support circuits.
-*     * Pin 1/TX/TXD0: Any serial transmissions from the ESP8266 will interfere.
-*     * Pin 3/RX/RXD0: Any serial transmissions to the ESP8266 will interfere.
-*   * ESP-01 modules are tricky. We suggest you use a module with more GPIOs
-*     for your first time. e.g. ESP-12 etc.
-*/
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <ir_Gree.h>
 
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-IRGreeAC ac(kIrLed);  // Set the GPIO to be used for sending messages.
+IRGreeAC ac(0);
+
+void onChange() {
+    static unsigned long t0;
+    unsigned long t = micros();
+    Serial.println(t - t0);
+    t0 = t;
+}
 
 void printState() {
-  // Display the settings.
-  Serial.println("GREE A/C remote is in the following state:");
-  Serial.print("  ");
-  Serial.println(ac.toString());
-  // Display the encoded IR sequence.
-  unsigned char* ir_code = ac.getRaw();
-  Serial.print("IR Code: 0x");
-  for (uint8_t i = 0; i < kGreeStateLength; i++)
-    Serial.print(ir_code[i], HEX);
-  Serial.println();
+    Serial.println("GREE A/C remote is in the following state:");
+    Serial.print("  ");
+    Serial.println(ac.toString());
+    uint8_t *ir_code = ac.getRaw();
+    Serial.print("IR Code: 0x");
+    for (uint8_t i = 0; i < kGreeStateLength; i++)
+        Serial.print(ir_code[i], HEX);
+    Serial.println();
+
+    pinMode(1, INPUT);
+    // attachInterrupt(digitalPinToInterrupt(2), onChange, CHANGE);
 }
 
 void setup() {
-  ac.begin();
-  Serial.begin(115200);
-  delay(200);
-  ac.calibrate();
+    ac.begin();
+    Serial.begin(115200);
+    delay(200);
+    ac.calibrate();
 
-  // Set up what we want to send. See ir_Gree.cpp for all the options.
-  // Most things default to off.
-  Serial.println("Default state of the remote.");
-  printState();
-  Serial.println("Setting desired state for A/C.");
-  ac.on();
-  ac.setFan(1);
-  // kGreeAuto, kGreeDry, kGreeCool, kGreeFan, kGreeHeat
-  ac.setMode(kGreeCool);
-  ac.setTemp(24);  // 16-30C
-  ac.setSwingVertical(false, kGreeSwingAuto);
-  ac.setXFan(false);
-  ac.setLight(true);
-  ac.setSleep(false);
-  ac.setTurbo(false);
+    Serial.println("Default state of the remote.");
+    printState();
+    Serial.println("Setting desired state for A/C.");
+    ac.on();
+    ac.setFan(1);
+    ac.setMode(kGreeCool);
+    ac.setTemp(24);
+    ac.setSwingVertical(false, kGreeSwingAuto);
+    ac.setXFan(false);
+    ac.setLight(true);
+    ac.setSleep(false);
+    ac.setTurbo(false);
 }
 
 void loop() {
-  // Now send the IR signal.
-// #if SEND_GREE
-  Serial.println("Sending IR command to A/C ...");
-  ac.send();
-// #endif  // SEND_GREE
-  printState();
-  delay(5000);
+    // unsigned long t0 = micros();
+    // int ledValue = 0;
+    // for (int i = 0; i < sizeof seq / sizeof seq[0]; ++i) {
+    //     unsigned long t1 = t0 + seq[i];
+    //     while (micros() < t1);
+    //     digitalWrite(0, ledValue = !ledValue);
+    //     t0 = t1;
+    // }
+    // digitalWrite(0, ledValue = 0);
+    // delay(1000);
+
+    delay(2000);
+    Serial.println("Sending IR command to A/C ...");
+    ac.send();
+    printState();
 }
