442771e687cb10631a519dc679253f09088ef8a9
archibate <1931127624@qq.com>
2025-09-14 17:10:50 +0800

    esp mouse add time idle

--- ESPMouse/src/main.cpp
+++ ESPMouse/src/main.cpp
@@ -1,92 +1,98 @@
 #include <Arduino.h>
-#include "BatteryPercent.h"
 // #include <BleMouse.h>
-// #include <esp_sleep.h>
+#include <esp_sleep.h>
+#include "BatteryPercent.h"
 
 // BleMouse bleMouse("ESPMouse", "Espressif", 100);
 
-const int CLK = 7;
-const int DT = 6;
+const int CLK = 2;
+const int DT = 3;
 const int PWR = A0;
 
 bool connected = false;
-volatile bool changed = false;
 int idle = 0;
+volatile bool changed = false;
 
 
-void onClkRise()
+void IRAM_ATTR onBatteryUpdate()
 {
-    // uint32_t pins = GPIO.in.data;
-    // bool clk = pins & (1 << CLK);
-    // bool dt = pins & (1 << DT);
-    // if (!clk) {
-    //     return;
-    // }
-    // int direction;
-    // if (dt == LOW) {
-    //     direction = 1;
-    // } else {
-    //     direction = -1;
-    // }
-    // digitalPinToAnalogChannel(PWR);
-    //
-    // if (connected) {
-    //     bleMouse.move(0, 0, direction, 0);
-    // }
+    int percent = batteryVoltageToPercentage(analogReadMilliVolts(PWR) * 10);
+    if (connected) {
+        // bleMouse.setBatteryLevel(percent);
+    }
 }
 
-        
+void IRAM_ATTR onClkRise()
+{
+    uint32_t pins = GPIO.in.data;
+    bool clk = pins & BIT(CLK);
+    bool dt = pins & BIT(DT);
+    if (!clk) {
+        return;
+    }
+    int direction;
+    if (dt == LOW) {
+        direction = 1;
+    } else {
+        direction = -1;
+    }
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+    // if (connected) {
+        // bleMouse.move(0, 0, direction, 0);
+    // }
+
+    changed = true;
+}
 
 void setup() {
-    // pinMode(CLK, INPUT);
-    // pinMode(DT, INPUT);
-    // attachInterrupt(CLK, onClkRise, RISING);
-    //
-    // pinMode(LED_BUILTIN, OUTPUT);
-    // digitalWrite(LED_BUILTIN, LOW);
-    //
-    // bleMouse.begin();
-    //
-    // esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
-    // esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
-    // esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
-    // setCpuFrequencyMhz(24);
+    pinMode(CLK, INPUT);
+    pinMode(DT, INPUT);
+    attachInterrupt(CLK, onClkRise, RISING);
 
-    Serial.begin();
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    // bleMouse.begin();
+
+    hw_timer_t *batteryTimer = timerBegin(1, 80, true);
+    timerAttachInterrupt(batteryTimer, onBatteryUpdate, false);
+    timerAlarmWrite(batteryTimer, 5'000'000, true);
+    timerAlarmEnable(batteryTimer);
+
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+    setCpuFrequencyMhz(80);
 }
 
 void loop() {
-    delay(500);
-    int percent = batteryVoltageToPercentage(analogReadMilliVolts(PWR) * 10);
-    Serial.println(percent);
-
     // if (!connected) {
     //     if (!bleMouse.isConnected()) {
     //         digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     //         Serial.println("connecting...");
+    //         delay(500);
     //         return;
     //     } else {
     //         connected = true;
-    //         digitalWrite(LED_BUILTIN, HIGH);
+    //         digitalWrite(LED_BUILTIN, LOW);
     //     }
     // }
-    //
-    // int latency = 1'000;
-    // if (idle > 6'000) {
-    //     esp_deep_sleep_enable_gpio_wakeup(1 << CLK,
-    //                                       digitalRead(CLK) ?
-    //                                       ESP_GPIO_WAKEUP_GPIO_LOW :
-    //                                       ESP_GPIO_WAKEUP_GPIO_HIGH);
-    //     esp_deep_sleep_start();
-    // }
-    //
-    // delay(latency);
-    //
-    // bool chg = changed;
-    // changed = 0;
-    // if (chg) {
-    //     idle = 0;
-    // } else {
-    //     idle += latency;
-    // }
+
+    delay(1000);
+
+    if (changed) {
+        changed = false;
+        idle = 0;
+    } else {
+        ++idle;
+    }
+
+    if (idle > 5) {
+        esp_deep_sleep_enable_gpio_wakeup(BIT(CLK),
+                                          digitalRead(CLK) ?
+                                          ESP_GPIO_WAKEUP_GPIO_LOW :
+                                          ESP_GPIO_WAKEUP_GPIO_HIGH);
+        esp_deep_sleep_start();
+    }
 }
